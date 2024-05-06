c56dcda5809ba5d5647e45b6252fdd03ae83b7f3
EDUARDO FREITAS MANGELI DE BRITO <06960720761@professores.ibmec.edu.br>
2024-05-06 15:03:51 -0300

    laços javascript

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ dados.forEach(
         console.log(elemento.nome);
     }
 );
+
+console.log("Usando o while", "--------------------------------------------------------");
+
+let indice = 0;
+
+while (indice < dados.length){
+    console.log("A posicição de", dados[indice].nome, "é", dados[indice].posicao);
+    indice++;
+}
+
+console.log("Usando o for", "--------------------------------------------------------");
+
+for (let i = 0; i < dados.length ; i++){
+    console.log(`A altura de ${dados[i].nome} é ${dados[i].altura}`);
+}
+
+console.log("Usando for...of", "--------------------------------------------------------");
+
+for (const atleta of dados){
+    console.log(`O nome completo de ${atleta.nome} é ${atleta.nome_completo}`);
+}
